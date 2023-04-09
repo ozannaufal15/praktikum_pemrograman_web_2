80187ead4fc60bd003ecce7eb28062d90a260074
ozannaufal15 <ozan.naufal15@gmail.com>
2023-04-09 09:58:31 +0800

    praktikum 3

--- PRAKTIKUM3/PRAK303/PRAK303.php
+++ PRAKTIKUM3/PRAK303/PRAK303.php
@@ -5,18 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        img{
+            height: 50px;
+        }
+        p{
+            font-size: 50px;
+        }
+    </style>
 </head>
 <body>
     <form action="" method="post">
         <div>
-            <label for="name1">Nama 1</label>
-            <input type="text" id="name1" name="name1">
+            <label for="val1">Batas Bawah :</label>
+            <input type="number" id="val1" name="val1">
         </div>
         <div>
-            <label for="name2">Nama 2</label>
-            <input type="text" id="name2" name="name2">
+            <label for="val2">Batas Atas :</label>
+            <input type="number" id="val2" name="val2">
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit">Cetak</button>
     </form>
+    <div>
+        <p>
+    <?php 
+        if(isset($_POST['val1']) && isset($_POST['val2'])){
+            if($_POST['val1']!="" && $_POST['val2']!=""){
+                $low = $_POST['val1'];
+                $high = $_POST['val2'];
+
+                do{
+                    if(($low+7)%5==0) echo "<img src=\"../PRAK303/star-images-9441.png\">";
+                    else echo " ".$low." ";
+                    $low++;
+                }while($low <= $high);
+            }
+        }
+    ?>
+        </p>
+    </div>
 </body>
 </html>
